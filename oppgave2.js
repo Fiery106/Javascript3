3d5e933cdf8ec2842564e7d4e439adc9e6633d11
Fiery106 <fiery106@gmail.com>
2025-04-25 10:59:21 +0200

    Oppgave 2 ferdig
    Selv om det trenges ikke, jeg sikret for at man kan også regne verdien i alle tre temperaturene :)

---
+++ oppgave2.js
@@ -0,0 +1,33 @@
+function regnTall() {
+    let verdi = document.getElementById("input_feltet").value;
+    let temperatur = document.getElementById("velg_temperatur").value;
+
+
+    document.getElementById("demo").innerHTML = verdi + " grader " + temperatur + " tilsvarer:" 
+
+
+    console.log(temperatur)
+    if (temperatur == "Fahrenreit") {
+        let celsius = (verdi - 32) / (9/5)
+        let kelvin = (verdi - 32) * (5/9) + 273.15
+
+        document.getElementById("sum1").innerHTML = celsius + " grader Celsius"
+        document.getElementById("sum2").innerHTML = kelvin + " grader Kelvin"
+
+
+    } else if (temperatur == "Celsius") {
+        let fahrenreit = verdi * 9/5 + 32
+        let kelvin = verdi + 273.15
+
+        document.getElementById("sum1").innerHTML = fahrenreit + " grader Fahrenreit"
+        document.getElementById("sum2").innerHTML = kelvin + " grader Kelvin"
+
+
+    } else {
+        let fahrenreit = verdi * 9/5 - 459.67
+        let celsius = verdi - 273.15
+
+        document.getElementById("sum1").innerHTML = fahrenreit + " grader Fahrenreit"
+        document.getElementById("sum2").innerHTML = celsius + " grader Celsius"
+    }
+}
